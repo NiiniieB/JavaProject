0a5d74bbfbaafb15d8904d712a55a375a39a1ce8
Niiniie <Melanie_Betourne_student2019@wilder.school>
2020-01-21 15:19:23 +0100

    add QuestionAnswer

---
+++ src/main/java/com/QuestionAnswer.java
@@ -0,0 +1,16 @@
+package com;
+
+//import java.util.ArrayList;
+//import java.util.List;
+
+public class QuestionAnswer {
+//    List<String> quesAnswer = new ArrayList<String>();
+//   quesAnswer.add("Un endroit où vivent toutes les sortes d'animaux est une arche de ?","noe");
+//   quesAnswer.add("D'après un proverbe bien connu, le malheur des uns fait le bonheur des ?","autres");
+//   quesAnswer.add("En France, quel numéro de téléphone doit-on composer pour joindre la police ?","17");
+//   quesAnswer.add("Le sirop d'orgeat est un sirop qui contient de ?","amande");
+//    int i = 0;
+//   for(i: quesAnswer ) {
+//        System.out.println(i);
+//    }
+}
